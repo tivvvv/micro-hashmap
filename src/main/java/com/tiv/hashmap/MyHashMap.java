b6b8200220edf100bae28f4f28284981e6ab2666
tivvvv <18612553935@163.com>
2025-03-31 13:23:07 +0800

    feat: 实现put方法.

--- src/main/java/com/tiv/hashmap/MyHashMap.java
+++ src/main/java/com/tiv/hashmap/MyHashMap.java
@@ -1,8 +1,21 @@
 package com.tiv.hashmap;
 
+import java.util.ArrayList;
+
 public class MyHashMap<K, V> {
 
+    ArrayList<Node<K, V>> table = new ArrayList<>();
+
     public V put(K key, V value) {
+        for (Node<K, V> node : table) {
+            if (node.key.equals(key)) {
+                V oldValue = node.value;
+                node.value = value;
+                return oldValue;
+            }
+        }
+        Node<K, V> newNode = new Node<>(key, value);
+        table.add(newNode);
         return null;
     }
 
@@ -12,5 +25,16 @@ public class MyHashMap<K, V> {
 
     public V remove(K key) {
         return null;
+    }
+
+    class Node<K, V> {
+
+        K key;
+        V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
