1fd0db147d907ec0c5a5ded1bd8177c938d27802
tivvvv <18612553935@163.com>
2025-03-31 13:15:39 +0800

    feat: 定义map基本结构.

---
+++ src/main/java/com/tiv/hashmap/MyHashMap.java
@@ -0,0 +1,16 @@
+package com.tiv.hashmap;
+
+public class MyHashMap<K, V> {
+
+    public V put(K key, V value) {
+        return null;
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public V remove(K key) {
+        return null;
+    }
+}
