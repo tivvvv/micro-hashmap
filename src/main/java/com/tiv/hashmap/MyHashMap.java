7efd089d5755849d8f0e31ccdaf1158c8ebaa66f
tivvvv <18612553935@163.com>
2025-04-01 08:49:24 +0800

    feat: 基于hash重写put方法.

--- src/main/java/com/tiv/hashmap/MyHashMap.java
+++ src/main/java/com/tiv/hashmap/MyHashMap.java
@@ -1,52 +1,53 @@
 package com.tiv.hashmap;
 
-import java.util.ArrayList;
-
 public class MyHashMap<K, V> {
 
-    ArrayList<Node<K, V>> table = new ArrayList<>();
+    private Node<K, V>[] table = new Node[16];
 
     public V put(K key, V value) {
-        for (Node<K, V> node : table) {
+        int keyIndex = indexOf(key);
+        Node<K, V> node = table[keyIndex];
+        if (node == null) {
+            table[keyIndex] = new Node<>(key, value);
+            return null;
+        }
+        while (true) {
             if (node.key.equals(key)) {
                 V oldValue = node.value;
                 node.value = value;
                 return oldValue;
             }
+            if (node.next == null) {
+                node.next = new Node<>(key, value);
+                return null;
+            }
+            node = node.next;
         }
-        Node<K, V> newNode = new Node<>(key, value);
-        table.add(newNode);
-        return null;
     }
 
     public V get(K key) {
-        for (Node<K, V> node : table) {
-            if (node.key.equals(key)) {
-                return node.value;
-            }
-        }
+
         return null;
     }
 
     public V remove(K key) {
-        for (int i = 0; i < table.size(); i++) {
-            Node<K, V> node = table.get(i);
-            if (node.key.equals(key)) {
-                Node<K, V> removed = table.remove(i);
-                return removed.value;
-            }
-        }
+
         return null;
     }
 
     public int size() {
-        return table.size();
+        return 0;
+    }
+
+    private int indexOf(Object key) {
+        return key.hashCode() % table.length;
     }
 
     class Node<K, V> {
 
         K key;
         V value;
+        Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
